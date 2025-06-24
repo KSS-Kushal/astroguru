fce2bc176a9130c3c5a7f45ad5764c7353946f83
Kushal Ghosh <kushalghosh075@gmail.com>
2025-06-24 22:33:59 +0530

    cache kundli and chart

--- src/main/java/com/kss/astrologer/AstrologerApplication.java
+++ src/main/java/com/kss/astrologer/AstrologerApplication.java
@@ -2,8 +2,10 @@ package com.kss.astrologer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class AstrologerApplication {
 
 	public static void main(String[] args) {
